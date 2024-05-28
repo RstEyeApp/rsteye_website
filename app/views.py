375deb1c40324955a642aba11ce4e02cd57c5ba4
Prince Roshan <princekrroshan01@gmail.com>
2024-05-28 15:03:09 +0530

    Add admin route to ask for credential before uploading the software

--- app/views.py
+++ app/views.py
@@ -1,8 +1,18 @@
-from flask import render_template, request, redirect, url_for, session, flash, Blueprint, send_from_directory
-from werkzeug.utils import secure_filename
 import os
 
+from flask import render_template, request, redirect, url_for, session, flash, Blueprint, send_from_directory
+from werkzeug.utils import secure_filename
+from werkzeug.security import generate_password_hash, check_password_hash
+from functools import wraps
+from dotenv import load_dotenv
+
+# Load environment variables from .env
+load_dotenv()
+
 main = Blueprint("main", __name__)
+
+admin_username = os.getenv('ADMIN_USERNAME')
+admin_password = os.getenv('ADMIN_PASSWORD')
 
 
 @main.route("/")
@@ -23,27 +33,64 @@ UPLOAD_FOLDER_ABSOLUTE = os.path.join(os.path.dirname(os.path.abspath(__file__))
 # Ensure the upload directory exists
 os.makedirs(UPLOAD_FOLDER_ABSOLUTE, exist_ok=True)
 
+# Function to check if user is authenticated as admin
+def login_required_admin(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        print(list(session))
+        if 'username' not in session or 'password' not in session or \
+           session['username'] != admin_username or \
+            admin_password != session['password']:
+            print("sjhsjh kyu ayya be")
+            return redirect(url_for('main.login_admin'))
+        return f(*args, **kwargs)
+    return decorated_function
+
+# Login page for admin
+@main.route('/login_admin', methods=['GET', 'POST'])
+def login_admin():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        print(username,password,admin_password,admin_username)
+        if username == admin_username and admin_password == password:
+            session['username'] = username
+            session['password'] = password
+            print("indie")
+            return redirect(url_for('main.upload_file'))
+    return '''
+    <form method="post">
+        <p><input type=text name=username>
+        <p><input type=password name=password>
+        <p><input type=submit value=Login>
+    </form>
+    '''
+
+# Logout route for admin
+@main.route('/logout_admin')
+def logout_admin():
+    session.pop('username', None)
+    session.pop('password', None)
+    return redirect(url_for('login_admin'))
+
+
 @main.route('/upload', methods=['GET', 'POST'])
+@login_required_admin
 def upload_file():
     if request.method == 'POST':
-        # Check if the post request has the file part
         if 'file' not in request.files:
             flash('No file part')
             return redirect(request.url)
         file = request.files['file']
-        # If the user does not select a file, the browser submits an
-        # empty file without a filename
         if file.filename == '':
             flash('No selected file')
             return redirect(request.url)
         if file:
-            try:
-                filename = secure_filename(file.filename)
-                file.save(os.path.join(UPLOAD_FOLDER_ABSOLUTE, filename))
-                uploaded_file_url = url_for('main.uploaded_file', filename=filename)
-                return redirect(uploaded_file_url)
-            except Exception as e:
-                print("Error saving file:", e)
+            filename = secure_filename(file.filename)
+            # Create the UPLOAD_FOLDER directory if it doesn't exist
+            os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+            file.save(os.path.join(UPLOAD_FOLDER, filename))
+            return redirect(url_for('main.uploaded_file', filename=filename))
     return '''
     <!doctype html>
     <title>Upload new File</title>
@@ -53,6 +100,7 @@ def upload_file():
       <input type=submit value=Upload>
     </form>
     '''
+
 
 @main.route('/uploads/<filename>')
 def uploaded_file(filename):
